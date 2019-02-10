15a1dc2d1732443a875db8232bc99e32b0473537
Nialas <Nialasagren@gmail.com>
2019-02-11 03:07:11 +1000

    Обучение вызову метода из класса.

---
+++ src/Java/JavaRush/Practic_001.java
@@ -0,0 +1,12 @@
+package Java.JavaRush;
+
+public class Practic_001 {
+    public static void main(String[] args) {
+        welcome();
+    }
+
+    private static void welcome() {
+        System.out.print("Hello, world!");
+    }
+}
+/* Вызов метода в main */
